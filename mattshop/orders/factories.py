a5a719878002640811c7126e40739e976f16aa82
Matt Jackson <me@mattjackson.co>
2024-04-18 20:46:31 +0100

    fixed a factory mistake

--- mattshop/orders/factories.py
+++ mattshop/orders/factories.py
@@ -1,20 +1,15 @@
-from datetime import datetime
-
 import factory
 
 from django.contrib.auth import get_user_model
 from factory.django import DjangoModelFactory
-from faker import Faker
 
 
 from mattshop.orders.models import Order, OrderItem
 from mattshop.products.factories import ProductFactory
 
-faker = Faker()
-
 class OrderItemFactory(DjangoModelFactory):
     product = factory.SubFactory(ProductFactory)
-    product_price = faker.pydecimal(min_value=0.01, max_value=1000, right_digits=2)
+    product_price = factory.Faker("pydecimal", min_value=0.01, max_value=1000, right_digits=2)
     quantity = factory.Faker('random_int')
 
     class Meta:
@@ -23,7 +18,7 @@ class OrderItemFactory(DjangoModelFactory):
 
 class OrderFactory(DjangoModelFactory):
     user = factory.SubFactory(get_user_model())
-    total_price = faker.pydecimal(min_value=0.01, max_value=1000, right_digits=2)
+    total_price = factory.Faker("pydecimal", min_value=0.01, max_value=1000, right_digits=2)
     items = factory.RelatedFactory(OrderItemFactory, factory_related_name='order')
 
     class Meta:
--- mattshop/products/factories.py
+++ mattshop/products/factories.py
@@ -2,15 +2,12 @@ from datetime import datetime
 
 import factory
 from factory.django import DjangoModelFactory
-from faker import Faker
 
 
 from mattshop.products.models import Product, ProductPrice
 
-faker = Faker()
-
 class ProductPriceFactory(DjangoModelFactory):
-    price = faker.pydecimal(min_value=0.01, max_value=1000, right_digits=2)
+    price = factory.Faker("pydecimal", min_value=0.01, max_value=1000, right_digits=2)
     effective_from = datetime(2020, 1, 1)
 
     class Meta:
@@ -18,8 +15,8 @@ class ProductPriceFactory(DjangoModelFactory):
 
 
 class ProductFactory(DjangoModelFactory):
-    name = "{}s".format(faker.street_name())  # looks sort of like a product name?
-    quantity_in_stock = faker.pyint(min_value=0, max_value=10)
+    name = factory.Faker("street_name")  # looks sort of like a product name?
+    quantity_in_stock = factory.Faker("pyint", min_value=0, max_value=10)
     enabled = True
     prices = factory.RelatedFactory(ProductPriceFactory, factory_related_name='product')
 
